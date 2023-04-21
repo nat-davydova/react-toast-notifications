882cbae088514aeb451d51b6ae82acb2b4bf6114
nat-davydova <natti.davydova@gmail.com>
2023-04-21 22:44:02 +0300

    feat: add notification toggle buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <div className="btn-container">
+        <button className="success-btn">Success</button>
+        <button className="info-btn">Info</button>
+        <button className="warning-btn">Warning</button>
+        <button className="error-btn">Error</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
